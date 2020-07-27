8209bfd5428efdd8e6b9735962ac4ce8bdf3bec1
margare-jihua <margaret.jihua@gmail.com>
2020-07-27 09:24:43 -0700

    Fetch the REST Countries API

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# national-flag-game
+# National Flag Game
+
 This is a game of guessing national flags
+
+## Free API
+From the [REST Countries](https://restcountries.eu/)
+
+Actural API Link: https://restcountries.eu/rest/v2/all
--- js/app.js
+++ js/app.js
@@ -1,1 +1,9 @@
-console.log('This is working');
+fetch("https://restcountries.eu/rest/v2/all")
+.then(response => {
+    return response.json()
+})
+.then(jsonData => {
+    console.log(jsonData.length);
+    let af = jsonData[0].flag
+    document.querySelector('img').setAttribute('src', af)
+})
