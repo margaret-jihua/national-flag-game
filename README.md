dd5548f5564c59104b454b4e6b37dc66ad86e2a5
margare-jihua <margaret.jihua@gmail.com>
2020-07-27 23:06:34 -0700

    Present 3 random countries with the flag country in quiz div

--- README.md
+++ README.md
@@ -4,9 +4,12 @@ This is a game of guessing national flags
 
 ## Intro of the game
 
-Flags help us to identify different countries around the world. A country's flag is made up of symbols, colours and sometimes phrases that represent its identity or character.Can you tell what country the flag belongs to?
+Flags help us to identify different countries around the world. A country's flag is made up of symbols, colors and sometimes phrases that represent its identity or character. Can you tell what country the flag belongs to?
 
-## Free API
+## 
+
+## Open Source API
+
 From [REST Countries](https://restcountries.eu/)
 
 Actural API Link: https://restcountries.eu/rest/v2/all
--- js/app.js
+++ js/app.js
@@ -2,24 +2,46 @@ let play = document.querySelector('#play-btn')
 let intro = document.querySelector('.intro')
 let quiz = document.querySelector('.quiz')
 let flag = document.querySelector('.flag')
-let a = document.querySelector('.a')
-let b = document.querySelector('.b')
-let c = document.querySelector('.c')
-let d = document.querySelector('.d')
+let a = document.querySelector('#a')
+let b = document.querySelector('#b')
+let c = document.querySelector('#c')
+let d = document.querySelector('#d')
 //let allCountriesNames = []
-let usedIndex = []
 let countries
 let numOfCountries
 let answerIndex
 let answer
-let optionA
-let optionB
-let optionC
-let optionD
+let options = []
+let usedIndex = []
 
+// Display random chosen countries along with the flag country
 function displayOptions() {
-    // assign answer to one of the options
-    
+    // get 3 random country index for choices
+    let i = 0
+    while (i < 3) {
+        let index = Math.floor(Math.random() * numOfCountries)
+        if(index !== answerIndex && !options.includes(index)){
+            options.push(index)
+            i++
+        }
+    }
+    // choose a random index to put answer into options
+    let answerOption = Math.floor(Math.random() * 4)
+    options.splice(answerOption, 0, answerIndex)
+    // store chosen names into optionText[]
+    let optionText = []
+    for (let i = 0; i < 4; i++) {
+        let text = countries[options[i]]["name"]
+        console.log(text);
+        optionText.push(text)
+    }
+    console.log(answerIndex, options)
+    console.log(optionText);
+    // display country names according to index stored in option[]
+    a.textContent = optionText[0]
+    b.textContent = optionText[1]
+    c.textContent = optionText[2]
+    d.textContent = optionText[3]  
 }
 
 // Fetch info from API
