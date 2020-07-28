9b6f13a45284ecdcb22f9a66d0c6d36b6e57ec7f
margare-jihua <margaret.jihua@gmail.com>
2020-07-27 21:06:28 -0700

    Set up quiz layout and display flag picture

--- js/app.js
+++ js/app.js
@@ -1,15 +1,57 @@
 let play = document.querySelector('#play-btn')
 let intro = document.querySelector('.intro')
+let quiz = document.querySelector('.quiz')
+let flag = document.querySelector('.flag')
+let a = document.querySelector('.a')
+let b = document.querySelector('.b')
+let c = document.querySelector('.c')
+let d = document.querySelector('.d')
+//let allCountriesNames = []
+let usedIndex = []
+let countries
+let numOfCountries
+let answerIndex
+let answer
+let optionA
+let optionB
+let optionC
+let optionD
 
+function displayOptions() {
+    // assign answer to one of the options
+    
+}
+
+// Fetch info from API
 fetch("https://restcountries.eu/rest/v2/all")
 .then(response => {
     return response.json()
 })
 .then(jsonData => {
-    console.log(jsonData.length)
-    let af = jsonData[0].flag
+    countries = jsonData
+
+    // get total number of countries
+    numOfCountries = countries.length
+
+    // get random index and set up quiz
+    answerIndex = Math.floor(Math.random() * numOfCountries)
+    flag.setAttribute('src', countries[answerIndex]["flag"])
+    answer = countries[answerIndex]["name"]
+    
+    // assign value to options
+    displayOptions()
+
+    
+})
+.catch(err => {
+    console.log("there was an error fetching the results")
+    console.log(err)
 })
 
+// Click [Let's Play] Button
 play.addEventListener('click', function() {
     intro.style.display = 'none'
+    setTimeout(function(){
+        quiz.style.display = 'block'
+    },100)
 })
